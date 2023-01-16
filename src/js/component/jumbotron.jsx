59cc31bd9a1335050ab837b1ec38c71fd0e2d951
Jessie <jessicabrinegar@outlook.com>
2023-01-15 19:10:25 -0600

    Update jumbotron.jsx

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -7,7 +7,7 @@ export const Jumbotron = () => {
             <h1 className="display-4">A Warm Welcome!</h1>
             <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos iusto debitis hic atque, ex, ipsam quia vitae tenetur nam magni, laboriosam voluptatum non. Saepe blanditiis, vero obcaecati totam iste aut!</p>
             <p className="lead">
-                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
             </p>
         </div>
     );
